4a1f475cfe72650b9aea3e1087712a0093684265
Pamode <ebenezerukwe@gmail.com>
2024-07-12 22:15:09 +0100

    Link updated from Anchor tag

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { BiCart, BiMenu, BiSearch } from "react-icons/bi"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     const [widthCheck, setWidthCheck] = useState(window.innerWidth)
@@ -35,8 +36,8 @@ const Navbar = () => {
             {/* Cart */}
             <div className="flex gap-4 sm:ml-[55%] md:ml-0 ml-[35%]">
                 <button><BiSearch className="w-8 h-8 outline rounded-full outline-gray-300/30 outline-2 p-1" title="search"/></button>
-                <a href="/cart" className="flex"><BiCart className="w-8 h-8 outline rounded-full outline-gray-300/30 outline-2 p-1 relative" title="cart"/>
-                <p className="bg-black text-white absolute rounded-3xl px-1 ml-5 text-xs">10</p></a>
+                <Link to='/cart' className="flex"><BiCart className="w-8 h-8 outline rounded-full outline-gray-300/30 outline-2 p-1 relative" title="cart"/>
+                <p className="bg-black text-white absolute rounded-3xl px-1 ml-5 text-xs">10</p></Link>
             </div>
         </div>
         {/* Ad */}
